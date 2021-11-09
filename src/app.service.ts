d79ab0524f862ea6e7070dac0745846f428152c6
Cezary Stroczyński <cezary.stroczynski@rumblefish.pl>
2021-11-09 22:37:54 +0100

    mm

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World! I'm working with network: ${process.env.NETWORK_NAME} and I was made from master branch!`;
+    return `Hello World!!! I'm working with network: ${process.env.NETWORK_NAME} and I was made from master branch!`;
   }
 }
